4fb196e1a1651d5c396368fe8074893151928b03
anbreaker <antunez19@gmail.com>
2020-09-23 11:32:56 +0200

    continued...

--- es10/index.js
+++ es10/index.js
@@ -1,11 +1,11 @@
-// Optional Catch Binding
-
 let array = [1, 2, 3, [1, 2, 3, [1, 2, 3]]];
 
 console.log(array.flat(2));
 
 let array2 = [1, 2, 3, 4, 5];
 console.log(array2.flatMap((value) => [value, value * 2]));
+
+// trimStart
 
 let hello = '    Espacio en blanco al inicio';
 console.log(hello);
